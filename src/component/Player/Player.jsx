a8b0a6f76af5c5d80e37a743a78c9fc43c825947
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-11-08 22:58:39 +0600

    fetch the players data and render them as card in Player Component and Players component

--- src/component/Player/Player.jsx
+++ src/component/Player/Player.jsx
@@ -1,9 +1,65 @@
-const Player = () => {
+import PropTypes from "prop-types";
+import { FaUser } from "react-icons/fa6";
+import { BsFlagFill } from "react-icons/bs";
+
+const Player = ({ player }) => {
+  const { name, image, country, role, bowlingType, biddingPrice } = player;
   return (
-    <div>
-      <p>Player</p>
+    <div className="p-[1.56rem] border border-[#1313131A] rounded-[0.96rem]">
+      {/* picture of the player  */}
+      <figure className="w-full mb-[1.44rem]">
+        <img src={image} alt={name} className=" w-full object-contain" />
+      </figure>
+
+      {/* player name  */}
+      <h4 className="flex items-center gap-[0.96rem] text-[#131313] text-[1.2rem] font-semibold">
+        <span>
+          <FaUser />
+        </span>{" "}
+        {name}
+      </h4>
+
+      {/* player info  */}
+      <div className="flex justify-between items-center mt-[0.96rem] pb-[0.96rem] border-b border-[#1313131A]">
+        <p className="flex items-center gap-[0.72rem] text-[0.96rem] text-[#13131366]">
+          <span>
+            <BsFlagFill />
+          </span>{" "}
+          {country}
+        </p>
+        <p className="text-[#131313] text-[0.85rem] px-[0.96rem] py-[0.55rem] bg-[#1313130D] rounded-lg">
+          {role}
+        </p>
+      </div>
+
+      {/* rating text  */}
+      <h5 className="mt-[0.96rem] font-bold text-[0.96rem]">Rating</h5>
+
+      {/* bowling type  */}
+      <div className="flex justify-between items-center text-[0.96rem] mt-[0.96rem]">
+        <p className="text-[#131313] font-semibold">{bowlingType}</p>
+        <p className="text-[#131313B3]">{bowlingType}</p>
+      </div>
+
+      {/* selection info  */}
+      <div className="flex justify-between items-center gap-5 mt-[0.96rem]">
+        <p className="text-[#131313] font-semibold">Price: ${biddingPrice}</p>
+
+        <div>
+          <button
+            className="text-[#131313] text-[0.85rem] px-[0.96rem] py-[0.55rem] border border-[#1313131A] rounded-lg cursor-pointer"
+            type="button"
+          >
+            Choose Player
+          </button>
+        </div>
+      </div>
     </div>
   );
+};
+
+Player.propTypes = {
+  player: PropTypes.object.isRequired,
 };
 
 export default Player;
--- src/component/Players/Players.jsx
+++ src/component/Players/Players.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import Player from "../Player/Player";
 
 const Players = () => {
+  // state: for players collection
+  const [players, setPlayers] = useState([]);
+
+  // effect: handle the fetch data
+  useEffect(() => {
+    fetch("allPlayers.json")
+      .then((res) => res.json())
+      .then((data) => setPlayers(data.players));
+  }, []);
+
+  console.log(players);
+
   return (
-    <div>
-      <p>Im from Players</p>
-      <Player></Player>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[1.56rem]">
+      {players.map((player) => (
+        <Player key={player.playerId} player={player}></Player>
+      ))}
     </div>
   );
 };
