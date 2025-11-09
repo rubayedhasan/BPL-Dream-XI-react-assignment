2e9bb00e5facb66babd4584437d2bde8b3bfdefc
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-11-09 22:10:50 +0600

    make newsletter section responsive for all devices

--- src/component/FooterContainer/FooterContainer.jsx
+++ src/component/FooterContainer/FooterContainer.jsx
@@ -2,7 +2,7 @@ import Newsletter from "../Newsletter/Newsletter";
 
 const FooterContainer = () => {
   return (
-    <footer className="mt-[22.68rem]  bg-[#131313]">
+    <footer className="mt-[19.6rem] md:mt-[22.68rem]  bg-[#131313]">
       <div className="w-full md:w-11/12 mx-auto px-5 md:px-0 relative ">
         {/* newsletter section component: Newsletter  */}
         <Newsletter></Newsletter>
--- src/component/Newsletter/Newsletter.jsx
+++ src/component/Newsletter/Newsletter.jsx
@@ -3,21 +3,21 @@ import BtnBgShadow from "../../assets/images/bg-btn.png";
 
 const Newsletter = () => {
   return (
-    <div className="bg-[#ffffff26] border-2 border-white p-[1.44rem] rounded-[1.44rem] absolute -top-40 w-full">
+    <div className="bg-[#ffffff26] border-2 border-white p-[0.96rem] md:p-[1.44rem] rounded-[1.44rem] absolute -top-40 left-0 w-full">
       <div
-        className="flex justify-center items-center bg-white bg-no-repeat bg-cover rounded-[1.44rem] py-[5.28rem] w-full"
+        className="flex justify-center items-center bg-white bg-no-repeat bg-cover rounded-[1.44rem] py-[2.28rem] md:py-[3.28rem] lg:py-[5.28rem] w-full"
         style={{ backgroundImage: `url(${ShadowPicture})` }}
       >
-        <div className="flex flex-col items-center justify-center gap-[0.96rem]">
-          <h4 className="font-bold text-[1.92rem]">
+        <div className="flex flex-col items-center justify-center gap-[0.76rem] lg:gap-[0.96rem] text-center">
+          <h4 className="font-bold text-[1.5rem] lg:text-[1.92rem]">
             Subscribe to our Newsletter
           </h4>
-          <p className="font-medium text-[1.2rem] text-[#131313B3]">
+          <p className="font-medium text-[1rem] lg:text-[1.2rem] text-[#131313B3]">
             Get the latest updates and news right in your inbox!
           </p>
 
           {/* user email */}
-          <form className="flex items-center gap-[0.96rem] w-full">
+          <form className="flex flex-col md:flex-row items-center gap-[0.96rem] w-full mt-[1.44rem]">
             <div className="flex-1">
               <input
                 type="email"
@@ -29,7 +29,7 @@ const Newsletter = () => {
             </div>
             <div>
               <button
-                className="font-bold text-[0.96rem] text-[#131313] py-[0.76rem] px-[1.8rem] rounded-[0.72rem] bg-no-repeat bg-cover cursor-pointer inset-shadow-sm inset-shadow-[#1313134D]"
+                className="font-bold text-[0.96rem] text-[#131313] py-[0.76rem] px-[1.8rem] rounded-[0.72rem] bg-center bg-no-repeat bg-cover cursor-pointer inset-shadow-sm inset-shadow-[#1313134D]"
                 style={{ backgroundImage: `url(${BtnBgShadow})` }}
                 type="submit"
               >
