9e20e209657fed12c1c2e06b734934d86b639d83
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-11-10 16:56:08 +0600

    adding footer menus and copy right text section on FooterMegamenu component

--- src/component/FooterContainer/FooterContainer.jsx
+++ src/component/FooterContainer/FooterContainer.jsx
@@ -3,13 +3,20 @@ import Newsletter from "../Newsletter/Newsletter";
 
 const FooterContainer = () => {
   return (
-    <footer className="mt-[19.6rem] md:mt-[22.68rem]  bg-[#131313]">
-      <div className="w-full md:w-11/12 mx-auto px-5 md:px-0 relative ">
+    <footer className="bg-[#131313]">
+      <div className="w-full md:w-11/12 mx-auto mt-[19.6rem] md:mt-[22.68rem] px-5 md:px-0 relative ">
         {/* newsletter section component: Newsletter  */}
         <Newsletter></Newsletter>
 
         {/* footer section component: Footer  */}
         <FooterMegaMenu></FooterMegaMenu>
+      </div>
+
+      {/* footer copy-right text  */}
+      <div className="py-[1.92rem] border border-[#ffffff26] mt-[4.32rem]">
+        <p className="text-[0.96rem] text-[#ffffff99] text-center">
+          <small>@2025 Dream-11 All Rights Reserved.</small>
+        </p>
       </div>
     </footer>
   );
--- src/component/FooterMegaMenu/FooterMegaMenu.jsx
+++ src/component/FooterMegaMenu/FooterMegaMenu.jsx
@@ -1,4 +1,5 @@
 import FooterLogo from "../../assets/images/logo-footer.png";
+import BtnBgShadow from "../../assets/images/bg-btn.png";
 
 const FooterMegaMenu = () => {
   return (
@@ -15,8 +16,69 @@ const FooterMegaMenu = () => {
       </div>
 
       {/* footer mega menu  */}
-      <div className="mt-[3.84rem]">
-        <p>mega menu</p>
+      <div className="footer sm:footer-horizontal mt-[3.84rem]">
+        {/* about us block  */}
+        <div>
+          <h5 className="font-semibold text-[1.1rem] text-white">About Us</h5>
+          <p className="text-[0.96rem] text-[#ffffff99] mt-[0.96rem]">
+            We are a passionate team dedicated to providing the best services to
+            our customers.
+          </p>
+        </div>
+
+        {/* quick links block  */}
+        <nav>
+          <h6 className="font-semibold text-[1.1rem] text-white">
+            Quick Links
+          </h6>
+          <ul
+            className="list-disc list-inside text-[0.96rem] text-[#ffffff99] space-y-2.5 mt-[0.96rem]
+"
+          >
+            <li>
+              <a className="link link-hover">Home</a>
+            </li>
+            <li>
+              <a className="link link-hover">Services</a>
+            </li>
+            <li>
+              <a className="link link-hover">About</a>
+            </li>
+            <li>
+              <a className="link link-hover">Contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* subscribe block */}
+        <form>
+          <h6 className="font-semibold text-[1.1rem] text-white">Subscribe</h6>
+          <p className="text-[0.96rem] text-[#ffffff99] mt-[0.96rem]">
+            Subscribe to our newsletter for the latest updates.
+          </p>
+
+          {/* form field  */}
+          <div className="flex flex-col md:flex-row items-center gap-[0.96rem] w-full mt-[1.44rem]">
+            <div className="flex-1">
+              <input
+                type="email"
+                name=""
+                id=""
+                className="input text-[0.96rem] text-[#131313B3] bg-white border border-[#13131326] py-[1.35rem] px-[1.8rem] rounded-[0.72rem] w-full block"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div>
+              <button
+                className="font-bold text-[0.96rem] text-[#131313] py-[0.76rem] px-[1.8rem] rounded-[0.72rem] bg-center bg-no-repeat bg-cover cursor-pointer inset-shadow-sm inset-shadow-[#1313134D]"
+                style={{ backgroundImage: `url(${BtnBgShadow})` }}
+                type="submit"
+              >
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
